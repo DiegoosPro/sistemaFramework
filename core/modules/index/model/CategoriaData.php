78cea6abf69a615ff055be43c8e4d5a0a195d59d
DiegoosPro <d90554686@gmail.com>
2023-07-31 17:03:31 -0500

    corregido php y plugins y caprpeta buil

---
+++ core/modules/index/model/CategoriaData.php
@@ -0,0 +1,49 @@
+<?php
+class CategoriaData  {
+
+public static function getAllCategorias()
+{
+  try {
+    $sql = "SELECT * FROM tab_categorias 
+                ORDER BY catego_descripcion";
+    $conexion = conectaBaseDatos();
+    $stmt = $conexion->prepare($sql);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+      $lista = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      return $lista;
+    } else
+      return null;
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+    return null;
+  }
+}
+
+public static function getNombreCategoriaById($catego_id)
+{
+  try {
+    $sql = "SELECT catego_descripcion FROM tab_categorias
+              WHERE catego_id=:pcatego_id";
+    $conexion = conectaBaseDatos();
+    $stmt = $conexion->prepare($sql);
+    $stmt->bindparam(":pcatego_id", $catego_id);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+      $registro = $stmt->fetch(PDO::FETCH_ASSOC);
+      return $registro['catego_descripcion'];
+    } else {
+      return null;
+    }
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+    return null;
+  }
+}
+
+
+}
+
+
+?>
+
