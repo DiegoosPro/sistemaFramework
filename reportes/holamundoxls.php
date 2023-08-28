2a773fc70878fe1b375becd189d8692592b099c5
EXPERTO ITSI <experto@ITSI.EDU>
2023-08-28 17:13:23 -0500

    asta reportes xls con IMAGEN

--- reportes/holamundoxls.php
+++ reportes/holamundoxls.php
@@ -2,8 +2,16 @@
 
 require '../vendor/autoload.php';
 
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
+
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Worksheet;
+
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Style\Style;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
 
 $spreadsheet = new Spreadsheet();
 $activeWorksheet = $spreadsheet->getActiveSheet();
@@ -11,3 +19,25 @@ $activeWorksheet->setCellValue('A1', 'Hello World !');
 
 $writer = new Xlsx($spreadsheet);
 $writer->save('hello_world.xlsx');
+
+//**************************************** */
+
+$objDrawing = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
+        $objDrawing->setName('Water_Level');
+        $objDrawing->setDescription('Water_Level');
+        $objDrawing->setPath('../img/escudo.png');
+        $objDrawing->setHeight(74);
+        $objDrawing->setCoordinates('A1');
+        $objDrawing->setWorksheet($spreadsheet->getActiveSheet());
+
+
+//*********************************** */
+$minombre="holamundo";
+        $archivo='PromedioFinalMSV_'.$minombre.'.xlsx';
+        // Se manda el archivo al navegador web, con el nombre que se indica (Excel2007)
+       header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="' . $archivo . '"');
+        header('Cache-Control: max-age=0');
+
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $writer->save('php://output');
